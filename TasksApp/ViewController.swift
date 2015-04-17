4af94e430943c569d0521bff1c13c8f5fce9af98
Furano <dominicfurano@gmail.com>
2015-04-17 05:58:18 -0600

    Initial Commit

---
+++ TasksApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TasksApp
+//
+//  Created by Dominic Furano on 4/17/15.
+//  Copyright (c) 2015 Dominic Furano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
